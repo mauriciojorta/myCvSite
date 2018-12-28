c8fa374a34867a3e49a0886e5f5092787ed6b906
mauriciojorta <mauriciojorta94@gmail.com>
2018-12-28 15:11:04 +0100

    Updates

---
+++ precache-manifest.8b13040f65d595d172be3349aff1053a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "26332935c7c2e3c4d5aa3154f597b180",
+    "url": "/mycv/static/media/fotoCV.26332935.png"
+  },
+  {
+    "revision": "a535af63239fb12c81dc",
+    "url": "/mycv/static/js/runtime~main.a535af63.js"
+  },
+  {
+    "revision": "adf6d6c92abda3f596f5",
+    "url": "/mycv/static/js/main.adf6d6c9.chunk.js"
+  },
+  {
+    "revision": "7c9948c4a45384653c67",
+    "url": "/mycv/static/js/1.7c9948c4.chunk.js"
+  },
+  {
+    "revision": "adf6d6c92abda3f596f5",
+    "url": "/mycv/static/css/main.a74866c1.chunk.css"
+  },
+  {
+    "revision": "7c9948c4a45384653c67",
+    "url": "/mycv/static/css/1.9bc65e3a.chunk.css"
+  },
+  {
+    "revision": "e7f7a926aec08682c124901a4414090d",
+    "url": "/mycv/index.html"
+  }
+];
