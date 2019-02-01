cb2c5149db5b44aea2986e66ea08b27d0a49ed96
mauriciojorta <mauriciojorta94@gmail.com>
2019-02-01 20:33:52 +0100

    Some mistakes in the introduction fixed

--- src/components/cv_sections/Introduction.js
+++ src/components/cv_sections/Introduction.js
@@ -5,12 +5,11 @@ export default function Introduction() {
     <div id="introductionSection">
       <h1 className="display-4 mb-2">Who am I?</h1>
       <p className="lead">
-        <b>Software Engineer</b> graduate from the University of La Laguna
+        <b>Software Engineering</b> graduate from the University of La Laguna
         (Spain), with experience in the creation of software solutions with
         programming languages such as <b>C++, Java</b> and those of web
         development like <b>HTML, CSS and JavaScript</b>. I also possess
-        knowledge about <b>Software Engineering</b> and{' '}
-        <b>Software Design Patterns</b>.
+        knowledge about <b>Software Design Patterns</b>.
       </p>
     </div>
   );
